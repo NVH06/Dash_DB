a9a5cf8e317b4ca1ca4dd79e8bc800276e40604c
NVH06 <117838214+NVH06@users.noreply.github.com>
2022-11-15 16:12:36 +0100

    update main.py for Render (2)

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from dash import Dash
 from src.layout import create_layout
 from dash_bootstrap_components.themes import BOOTSTRAP
 
-def main() -> None:
-    app = Dash(external_stylesheets=[BOOTSTRAP])
-    server = app.server
-    app.title = "Dashboard"
-    app.layout = create_layout(app)
-    app.run()
+app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
+server = app.server
+app.title = "Dashboard"
+app.layout = create_layout(app)
+
 
 if __name__ == "__main__":
-    main()
-
+    app.run_server(debug=False)
