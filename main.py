677462593d72dbfeac5786f0f673f463d39f1cd8
NVH06 <117838214+NVH06@users.noreply.github.com>
2022-11-15 14:51:32 +0100

    update main.py for Render

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from dash import Dash
 from src.layout import create_layout
 from dash_bootstrap_components.themes import BOOTSTRAP
-#jefggjgis
 
 def main() -> None:
     app = Dash(external_stylesheets=[BOOTSTRAP])
+    server = app.server
     app.title = "Dashboard"
     app.layout = create_layout(app)
     app.run()
