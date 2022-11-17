b209759101a6198f3618e83fe7e11a6f0a5b2ca8
NVH06 <nvh.fiverr@gmail.com>
2022-11-17 10:22:10 +0100

    adjustments for Render

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from dash import Dash
 from src.layout import create_layout
 from dash_bootstrap_components.themes import BOOTSTRAP
+#jefggjgis
 
-app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
-server = app.server
-app.title = "Dashboard"
-app.layout = create_layout(app)
-
+def main() -> None:
+    app = Dash(external_stylesheets=[BOOTSTRAP])
+    app.title = "Dashboard"
+    app.layout = create_layout(app)
+    app.run()
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    main()
+
